70b54efd38028b8e72de2c33aa2926e7b1fa1f53
Claudio0996 <cvsilva391@gmail.com>
2025-09-26 15:18:12 -0300

    commit video

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   return (
     <>
       <video muted autoPlay loop width="80%">
-        <source src="/aniversario-orcafascio/video-aniversario.mp4" type="video/mp4" />
+        <source src="/video-aniversario.mp4" type="video/mp4" />
       </video>
     </>
   );
