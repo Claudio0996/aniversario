233b520d490ad9674fecd195f8b44cebf8cc55e3
Claudio0996 <cvsilva391@gmail.com>
2025-09-26 15:23:28 -0300

    ajuste base

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <video muted autoPlay loop width="80%">
-        <source src="/video-aniversario.mp4" type="video/mp4" />
+        <source src="/aniversario/video-aniversario.mp4" type="video/mp4" />
       </video>
     </>
   );
