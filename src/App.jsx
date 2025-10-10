87e1e4b6c46541492e5d9e1a4db2296090867a8d
Claudio0996 <cvsilva391@gmail.com>
2025-10-10 15:55:32 -0300

    Commit eliseu

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <video className="video" muted autoPlay loop width="100%">
+      <source src="/aniversario/video-aniversario.mp4" type="video/mp4" />
+    </video>
+  );
+}
+
+export default App;
