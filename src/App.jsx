96fd0f4ddfd472755a2640a6bc19df4b3f1fefc8
Claudio0996 <cvsilva391@gmail.com>
2025-10-15 08:44:44 -0300

    Black Friday

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <video className="video" muted autoPlay loop width="100%">
-      <source src="/aniversario/video-aniversario.mp4" type="video/mp4" />
-    </video>
-  );
+  return <img className="image" src="./bg-orcablack.png" alt="Logo da Black Friday" />;
 }
 
 export default App;
