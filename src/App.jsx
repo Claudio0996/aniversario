9307ce0f20c112d5e401c15374e5aad8f3e5895a
Claudio0996 <cvsilva391@gmail.com>
2025-10-15 09:32:08 -0300

    Commit ajuste atualização automática

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import "./App.css";
 
 function App() {
+  const [tick, setTick] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTick((prevTick) => prevTick + 1);
+    }, 5 * 600 * 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return <img className="image" src="./bg-orcablack.png" alt="Logo da Black Friday" />;
 }
 
