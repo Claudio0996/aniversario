51705dff3affaa1a11d52f416ac56ba7a868c4b1
Claudio0996 <cvsilva391@gmail.com>
2025-10-16 08:19:45 -0300

    Commit carrosel

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import { useEffect, useState } from "react";
-
 import "./App.css";
 
-function App() {
+export default function App() {
   const [tick, setTick] = useState(0);
+
+  const content = [
+    { type: "img", src: "bg-orcablack.png" },
+    { type: "video", src: "video-aniversario-alex.mp4" },
+    { type: "video", src: "video-aniversario-orcafascio.mp4" },
+  ];
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setTick((prevTick) => prevTick + 1);
-    }, 5 * 600 * 1000);
+      setTick((prevTick) => (prevTick + 1) % content.length);
+    }, 5 * 1000);
 
     return () => clearInterval(timer);
-  }, []);
+  }, [content.length]);
 
-  return <img className="image" src="./bg-orcablack.png" alt="Logo da Black Friday" />;
+  const current = content[tick];
+
+  return <>{current.type === "img" ? <img className="image" src={`./${current.src}?v=${tick}`} alt="Conteúdo dinâmico" /> : <video className="image" src={`./${current.src}?v=${tick}`} autoPlay muted loop />}</>;
 }
-
-export default App;
