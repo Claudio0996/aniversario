5809387d67a51f98590edb20da6e4719cbf5f61d
Claudio0996 <cvsilva391@gmail.com>
2025-10-24 16:31:52 -0300

    Ajustes aniversariante

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,17 @@ import "./App.css";
 function App() {
   const [tick, setTick] = useState(0);
 
+  const videos = [
+    {
+      src: "./video-1.mp4",
+      id: 1,
+    },
+    {
+      src: "./video-2.mp4",
+      id: 2,
+    },
+  ];
+
   useEffect(() => {
     const timer = setInterval(() => {
       setTick((prevTick) => prevTick + 1);
@@ -12,7 +23,15 @@ function App() {
     return () => clearInterval(timer);
   }, []);
 
-  return <img className="image" src={`./bg-orcablack.png`} alt="OrcaFascio Img" />;
+  let content;
+
+  if (tick % 2 === 0) {
+    content = <video src={videos[0].src} alt="aniversariante" autoPlay muted></video>;
+  } else {
+    content = <video src={videos[1].src} alt="aniversariante" autoPlay muted></video>;
+  }
+
+  return content;
 }
 
 export default App;
