cb80ada82751dc58e192598af2deca30231975e2
Claudio0996 <cvsilva391@gmail.com>
2025-10-28 07:58:42 -0300

    Ajustes black

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,7 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [tick, setTick] = useState(0);
-
-  const videos = [
-    {
-      src: "./video-1.mp4",
-      id: 1,
-    },
-    {
-      src: "./video-2.mp4",
-      id: 2,
-    },
-  ];
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setTick((prevTick) => prevTick + 1);
-    }, 30 * 1000);
-
-    return () => clearInterval(timer);
-  }, []);
-
-  let content;
-
-  if (tick % 2 === 0) {
-    content = <video src={videos[0].src} alt="aniversariante" autoPlay muted></video>;
-  } else {
-    content = <video src={videos[1].src} alt="aniversariante" autoPlay muted></video>;
-  }
-
-  return content;
+  return <img src="./bg-orcablack.png" className="image" />;
 }
 
 export default App;
