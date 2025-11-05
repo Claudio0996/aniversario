5c2c5fb366cd148f64cc38dc53caf619f1177a62
Claudio0996 <cvsilva391@gmail.com>
2025-11-05 16:37:44 -0300

    Video Giovanna

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,22 @@ import { useState, useEffect } from "react";
 
 import "./App.css";
 
-const content = [{ url: "./slide-1.jpg" }, { url: "./slide-2.jpg" }, { url: "./slide-3.jpg" }, { url: "./slide-4.jpg" }, { url: "./slide-5.jpg" }];
+// const content = [{ url: "./slide-1.jpg" }, { url: "./slide-2.jpg" }, { url: "./slide-3.jpg" }, { url: "./slide-4.jpg" }, { url: "./slide-5.jpg" }];
 
 function App() {
-  const [tick, setTick] = useState(0);
+  // const [tick, setTick] = useState(0);
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setTick((prev) => (prev + 1) % content.length);
-    }, 20000);
+  // useEffect(() => {
+  //   const timer = setInterval(() => {
+  //     setTick((prev) => (prev + 1) % content.length);
+  //   }, 20000);
 
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
+  //   return () => {
+  //     clearInterval(timer);
+  //   };
+  // }, []);
 
-  return <>{<img src={`${content[tick].url}?tick=${tick}`} className="image" />}</>;
+  return <video src={`./video-1.mp4`} className="video" autoPlay muted />;
 }
 
 export default App;
