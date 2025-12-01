c1d1e7152a9e5bc88e25846b7e424835d45f70a1
Claudio0996 <cvsilva391@gmail.com>
2025-12-01 08:09:57 -0300

    Ajuste aniversario jullya

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,25 @@ import { useState, useEffect } from "react";
 
 import "./App.css";
 
-const content = [
-  { url: "./image-1.png", type: "image" },
-  { url: "./image-2.png", type: "image" },
-];
+// const content = [
+//   { url: "./image-1.png", type: "image" },
+//   { url: "./image-2.png", type: "image" },
+// ];
 
 function App() {
-  const [tick, setTick] = useState(0);
+  // const [tick, setTick] = useState(0);
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setTick((prev) => (prev + 1) % content.length);
-    }, 30000);
+  // useEffect(() => {
+  //   const timer = setInterval(() => {
+  //     setTick((prev) => (prev + 1) % content.length);
+  //   }, 30000);
 
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
+  //   return () => {
+  //     clearInterval(timer);
+  //   };
+  // }, []);
 
-  return <>{<img src={`${content[tick].url}?tick=${tick}`} className="image"></img>}</>;
+  return <video src="video-1.mp4" className="video" autoPlay muted loop />;
 }
 
 export default App;
