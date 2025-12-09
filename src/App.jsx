31fd0280c7f26ef86dfbd4e62a3c8f941b57be3b
Claudio0996 <cvsilva391@gmail.com>
2025-12-09 08:06:02 -0300

    Ajustes aniversario

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,25 @@ import { useState, useEffect } from "react";
 
 import "./App.css";
 
-// const content = [
-//   { url: "./image-1.png", type: "image" },
-//   { url: "./image-2.png", type: "image" },
-// ];
+const content = [
+  { url: "./video-1.mp4", type: "video" },
+  { url: "./video-2.mp4", type: "video" },
+];
 
 function App() {
-  // const [tick, setTick] = useState(0);
+  const [tick, setTick] = useState(0);
 
-  // useEffect(() => {
-  //   const timer = setInterval(() => {
-  //     setTick((prev) => (prev + 1) % content.length);
-  //   }, 30000);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTick((prev) => (prev + 1) % content.length);
+    }, 30000);
 
-  //   return () => {
-  //     clearInterval(timer);
-  //   };
-  // }, []);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
 
-  return <video src="video-1.mp4" className="video" autoPlay muted loop />;
+  return <video src={`${content[tick].url}?=${tick}`} className="video" autoPlay muted loop />;
 }
 
 export default App;
