46edcd5a967afb2cb36a81253c0c8bf6e9e59569
Claudio <cvsilva391@gmail.com>
2026-01-02 07:24:44 -0300

    Atualização aniversário

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+import "./App.css";
+
+const content = [{ url: "./video-1.mp4", type: "video" }];
+
+function App() {
+  const [tick, setTick] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTick((prev) => (prev + 1) % content.length);
+    }, 30000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <video
+      src={`${content[tick].url}?=${tick}`}
+      className="video"
+      autoPlay
+      muted
+      loop
+    />
+  );
+}
+
+export default App;
