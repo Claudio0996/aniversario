2e5e4146b27c61c2f0426d08add27d7f532394e0
Claudio0996 <cvsilva391@gmail.com>
2026-03-03 13:40:46 -0300

    Adicionado notícias

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ const content = [
   { url: "./video-2.mp4", type: "video" },
   { url: "./image-2.png", type: "image" },
   { url: "./image-3.png", type: "image" },
+  { url: "./image-4.png", type: "image" },
 ];
 
 function App() {
