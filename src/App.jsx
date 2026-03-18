4650b54d6e2bc8db9e7bf0b1f242da4c8c1fb9ce
Claudio0996 <cvsilva391@gmail.com>
2026-03-18 08:39:10 -0300

    Ajustes conteúdo

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 
 const content = [
   { url: "./image-1.png", type: "image" },
-  { url: "./video-1.png", type: "video" },
+  { url: "./video-1.mp4", type: "video" },
   { url: "./image-2.png", type: "image" },
   { url: "./image-3.png", type: "image" },
   { url: "./image-4.png", type: "image" },
