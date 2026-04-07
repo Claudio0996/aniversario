63090e3339c3a47c85f75b23dc32f984d50076bb
Claudio0996 <cvsilva391@gmail.com>
2026-04-07 12:50:57 -0300

    mudanças

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import "./App.css";
 const content = [
   { url: "./image-1.png", type: "image" },
   { url: "./video-1.mp4", type: "video" },
-  { url: "./video-2.mp4", type: "video" },
   { url: "./image-2.png", type: "image" },
   { url: "./image-3.png", type: "image" },
   { url: "./image-4.png", type: "image" },
