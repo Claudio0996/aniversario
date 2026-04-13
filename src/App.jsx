731d98836993c81c82fd7face22dad044a120a22
Claudio0996 <cvsilva391@gmail.com>
2026-04-13 17:00:54 -0300

    Conteúdo terça

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ const content = [
   { url: "./image-1.png", type: "image" },
   { url: "./image-2.png", type: "image" },
   { url: "./image-3.png", type: "image" },
-  { url: "./video-1.mp4", type: "video" },
+  { url: "./image-4.png", type: "image" },
+  { url: "./image-5.png", type: "image" },
 ];
 
 function App() {
